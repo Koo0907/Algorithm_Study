3a77c00c66ae42b1e9e6489d2aaf15a20afac114
Koo Seung Hyun <rntmdgus09@gmail.com>
2022-04-24 13:21:31 +0900

    Greedy Algorithm ATM.cpp(백준 11399번) - 주석 추가 https://www.acmicpc.net/problem/11399

--- Baekjoon/Greedy/ATM.cpp
+++ Baekjoon/Greedy/ATM.cpp
@@ -18,13 +18,14 @@ int main() {
         arr.push_back(x);
     }
     
+    // ATM 이용 시간 오름차순 정렬
     sort(arr.begin(), arr.end());
 
     for(int i = 0; i < N; i++)
     {
         for(int j = 0; j <= i; j++)
         {
-            res += arr[j];
+            res += arr[j];  // 2중 반복 이용해서 누적 시간 계산
         }
     }
 
