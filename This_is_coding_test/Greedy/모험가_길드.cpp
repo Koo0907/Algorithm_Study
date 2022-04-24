f2ce445a0ed9efa5fd5c5661fab2b5a0ff880c5c
Koo Seung Hyun <rntmdgus09@gmail.com>
2022-04-24 12:20:58 +0900

    Greedy Algorithm 모험가 길드(p.311) - 주석 추가

--- This_is_coding_test/Greedy/모험가_길드.cpp
+++ This_is_coding_test/Greedy/모험가_길드.cpp
@@ -19,15 +19,16 @@ int main() {
         fear.push_back(x);
     }
 
+    // sort 메서드 이용해서 오름차순 정렬
     sort(fear.begin(), fear.end());
 
     for(vector<int>::iterator itr = fear.begin(); itr != fear.end(); itr++)
     {
-        num++;
-        if(num >= *itr)
+        num++;  // 현재 그룹의 모험가 수 증가
+        if(num >= *itr) // 현재 그룹의 모험가 수가 순회하고 있는 공포도 이상이라면 그룹으로 모험을 보낼 수 있음
         {
             res++;
-            num = 0;
+            num = 0;    // 현재 그룹의 모험가 수 초기화
         }
     }
 
