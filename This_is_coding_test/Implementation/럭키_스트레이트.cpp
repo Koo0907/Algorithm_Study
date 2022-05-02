117a24391e36de65f5e5c2cf3a4d425a5262beb9
Koo Seung Hyun <rntmdgus09@gmail.com>
2022-05-02 20:32:36 +0900

    Implementation Algorithm 럭키 스트레이트(p.321)

---
+++ This_is_coding_test/Implementation/럭키_스트레이트.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main() {
+
+    string str;
+    int sum1 = 0;
+    int sum2 = 0;
+
+    cin >> str;
+
+    // 왼쪽 부분의 각 자릿수의 합
+    for(int i = 0; i < (str.size() / 2); i++)
+    {
+        sum1 += (str[i] - '0');
+    }
+
+    // 오른쪽 부분의 각 자릿수의 합
+    for(int i = (str.size() / 2); i < str.size(); i++)
+    {
+        sum2 += (str[i] - '0');
+    }
+
+    // 각 합들이 같은지 검사
+    if(sum1 == sum2)
+    {
+        cout << "LUCKY" << endl;
+    }
+    else
+    {
+        cout << "READY" << endl;
+    }
+}
