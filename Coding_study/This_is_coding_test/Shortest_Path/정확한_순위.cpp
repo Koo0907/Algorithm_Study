241de17e45c3623a6fb33d3f1fe1d049c2ff1d97
Koo Seung Hyun <rntmdgus09@gmail.com>
2022-06-13 19:30:29 +0900

    Shortest_Path Algorithm 정확한 순위(p.386)

---
+++ Coding_study/This_is_coding_test/Shortest_Path/정확한_순위.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+
+#define INF 1e9 // 무한을 의미하는 값으로 10억을 설정
+#define MAX 501 // 최대 학생 수가 500개이므로 501을 MAX 값으로 설정
+
+using namespace std;
+
+// 사용할 변수 초기화
+int N = 0;
+int M = 0;
+int S1 = 0;
+int S2 = 0;
+int cnt = 0;
+
+// 최단거리 테이블
+int graph[MAX][MAX];
+
+int main()
+{
+    // 학생 수 N, 비교 횟수 M
+    cin >> N >> M;
+
+    // 최단거리 테이블 INF로 초기화
+    for(int i = 0; i < MAX; i++)
+    {
+        fill(graph[i], graph[i] + MAX, INF);
+    }
+
+    // 학생들의 성적 비교 데이터 입력
+    for(int i = 0; i < M; i++)
+    {
+        scanf("%d %d",&S1, &S2);
+        graph[S1][S2] = 1;
+    }
+
+    // 플로이드 워셜 알고리즘 수행
+    for(int k = 1; k <= N; k++)
+    {
+        for(int i = 1; i <= N; i++)
+        {
+            for(int j = 1; j <= N; j++)
+            {
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+            }
+        }
+    }
+
+    for(int i = 1; i <= N; i++)
+    {
+        int temp = 0;
+
+        // 각 번호의 열과 행을 순회
+        for(int j = 1; j <= N; j++)
+        {
+            // 순회중인 열과 행에 INF(성적을 비교한 적이 없다는 의미)가 없다면 temp 값 증가
+            if((graph[i][j] != INF) ||(graph[j][i] != INF))
+            {
+                temp++;
+            }
+        }
+
+        // temp 값이 graph[i][j]를 제외하고 INF가 없다면 성적의 순서를 알 수 있으므로 cnt 증가
+        if(temp == (N - 1))
+        {
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
